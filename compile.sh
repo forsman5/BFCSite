94934a193a1b13df9e4af593768a403f26355e87
Unknown <allegretti813@gmail.com>
2018-01-14 15:01:40 -0500

    Added photos page

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 html-partials-compiler ./html/index.html > ./dist/index.html
 html-partials-compiler ./html/findUs.html > ./dist/findUs.html
+html-partials-compiler ./html/photos.html > ./dist/photos.html
+

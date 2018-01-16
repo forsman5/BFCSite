69fa7f11dd87ce399e131374c7d186dc53a8404c
Joseph Forsman <jrforsman@gmail.com>
2018-01-15 19:19:59 -0500

    set up for gh-pages

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for file in ./html/*.html; do
     name=$(basename "$file" ".html")
-    html-partials-compiler ./html/$name.html > ./dist/$name.html
+    html-partials-compiler ./html/$name.html > ./docs/$name.html
 done
 
